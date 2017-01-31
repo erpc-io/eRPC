8aa8e85a11d6e92d8816cf131758e0009df8804a
Anuj Kalia <akalia@cs.cmu.edu>
2017-01-30 18:52:41 -0700

    Save work

--- src/util/huge_alloc.h
+++ src/util/huge_alloc.h
@@ -26,41 +26,58 @@ class HugeAllocator {
  private:
   static const size_t kMaxAllocSize = (256 * 1024 * 1024 * 1024ull);
 
-  /* Information about an SHM region. */
+  /// Information about an SHM region
   struct shm_region_t {
-    int key;        /* The key used to create the SHM region */
-    void *base_buf; /* The start address of the allocated SHM buffer */
-    size_t size;    /* The size in bytes of the allocated buffer */
+    const int key;        ///< The key used to create the SHM region
+    const void *alloc_buf; ///< The start address of the allocated SHM buffer
+    const size_t alloc_size; ///< The size in bytes of the allocated buffer
+    const size_t alloc_hugepages; ///< The number of allocated hugepages
 
-    void *cur_buf;         /* Pointer to the currently free hugepage */
-    size_t free_hugepages; /* The number of hugepages left in this region */
+    std::vector<bool> free_hugepage_vec; /// Bit-vector of free hugepages
 
-    shm_region_t(int key, void *buf, size_t size)
-        : key(key), base_buf(buf), size(size), cur_buf(buf) {
-      assert(size % kHugepageSize == 0);
-      free_hugepages = size / kHugepageSize;
+    /// For each index in \p free_hugepage_vec, store the number of contiguous
+    /// hugepages that were allocated using \p alloc_contiguous. This is
+    /// required do free allocated hugepages using just the address.
+    std::vector<size_t> nb_contig_vec;
+
+    size_t free_hugepages; ///< The number of hugepages left in this region
+
+    shm_region_t(int key, void *buf, size_t alloc_size)
+        : key(key), alloc_buf(buf), alloc_size(alloc_size),
+          alloc_hugepages(alloc_size / kHugepageSize) {
+      assert(alloc_size % kHugepageSize == 0);
+
+      /* Mark all hugepages as free */
+      free_hugepage_vec.resize(alloc_hugepages);
+      nb_contig_vec.resize(alloc_hugepages);
+      for (size_t i = 0; i < alloc_hugepages; i++) {
+        free_hugepage_vec[i] = true;
+        nb_contig_vec[i] = 0;
+      }
+
+      free_hugepages = alloc_hugepages;
     }
   };
 
-  SlowRand slow_rand; /* RNG to generate SHM keys */
-  size_t numa_node;   /* NUMA node on which all memory is allocated */
+  SlowRand slow_rand; ///< RNG to generate SHM keys
+  size_t numa_node;   ///< NUMA node on which all memory is allocated
 
   /*
    * SHM regions used by this allocator, in order of increasing allocation-time
    * size.
    */
   std::vector<shm_region_t> shm_list;
-  std::vector<void *> page_freelist; /* Currently free 4k pages */
+  std::vector<void *> page_freelist; ///< Currently free 4k pages
 
-  size_t tot_free_hugepages; /* Number of free hugepages over all SHM regions */
+  size_t tot_free_hugepages; ///< Number of free hugepages over all SHM regions
 
-  /*
+  /**
    * The size of the previous hugepage allocation made internally by this
    * allocator.
    */
   size_t prev_allocation_size;
-  size_t tot_memory_reserved;  /* Total hugepage memory reserved by allocator */
-  size_t tot_memory_allocated; /* Total memory allocated to users */
+  size_t tot_memory_reserved;  ///< Total hugepage memory reserved by allocator
+  size_t tot_memory_allocated; ///< Total memory allocated to users
 
  public:
   HugeAllocator(size_t initial_size, size_t numa_node)
@@ -82,7 +99,7 @@ class HugeAllocator {
   ~HugeAllocator() {
     /* Delete the created SHM regions */
     for (shm_region_t &shm_region : shm_list) {
-      delete_shm(shm_region.key, shm_region.base_buf);
+      delete_shm(shm_region.key, shm_region.alloc_buf);
     }
   }
 
@@ -108,17 +125,18 @@ class HugeAllocator {
 
       /*
        * If we are here, there is at least one SHM region with a free hugepage.
-       * Pick the smallest SHM region with a free hugepage and carve it into
-       * 4K pages. Note that multiple SHM regions can have free hugepages.
+       * Pick the smallest (wrt allocation size) SHM region with a free hugepage
+       * and carve it into 4K pages. Note that multiple SHM regions can have
+       * free hugepages.
        */
       for (shm_region_t &shm_region : shm_list) {
         if (shm_region.free_hugepages > 0) {
+          void *huge_buf = alloc_contiguous(shm_region, 1);
+          assert(huge_buf != nullptr); /* 1 hugepages should be available */
           for (size_t i = 0; i < kHugepageSize; i += kPageSize) {
-            void *page_addr = (void *)((char *)shm_region.cur_buf + i);
+            void *page_addr = (void *)((char *)huge_buf + i);
             page_freelist.push_back(page_addr);
           }
-
-          pop_hugepages(shm_region, 1);
 
           /* If we are here, we have a free 4K page */
           assert(page_freelist.size() > 0);
@@ -148,19 +166,21 @@ class HugeAllocator {
     size = round_up<kHugepageSize>(size);
     size_t reqd_hugepages = size / kHugepageSize;
 
+    /*
+     * Try to get contiguous hugepages from an existing SHM region, starting
+     * from the smallest (wrt allocation size).
+     */
     for (shm_region_t &shm_region : shm_list) {
-      if (shm_region.free_hugepages >= reqd_hugepages) {
-        void *hugebuf_addr = shm_region.cur_buf; /* Copy before popping */
-        pop_hugepages(shm_region, reqd_hugepages);
-
+      void *huge_buf = alloc_contiguous(shm_region, reqd_hugepages);
+      if (huge_buf != nullptr) {
         tot_memory_allocated += size;
-        return hugebuf_addr;
+        return huge_buf;
       }
     }
 
     /*
-     * If here, no existing SHM region has sufficient hugepages. Increase the
-     * allocation size, and ensure that we can allocate at least \p size.
+     * If we are here, no existing SHM region has sufficient hugepages. Increase
+     * the allocation size, and ensure that we can allocate at least \p size.
      */
     prev_allocation_size *= 2;
     while (prev_allocation_size < size) {
@@ -178,12 +198,54 @@ class HugeAllocator {
      * have enough space.
      */
     shm_region_t &shm_region = shm_list.back();
-    void *hugebuf_addr = shm_region.cur_buf; /* Copy before popping */
 
-    pop_hugepages(shm_region, reqd_hugepages);
+    /*
+     * This allocation must succeed - we allocated enough memory, and the SHM
+     * region is not fragmented yet.
+     */
+    void *hugebuf_addr = alloc_contiguous(shm_region, reqd_hugepages);
+    assert(hugebuf_addr != nullptr);
 
     tot_memory_allocated += size;
     return hugebuf_addr;
+  }
+
+  /// Free a hugepage memory region that was allocated using this allocator
+  void free_huge(void *huge_buf) {
+    if((size_t)huge_buf % kHugepageSize != 0) {
+      std::ostringstream xmsg;
+      xmsg << "eRPC HugeAllocator: free_huge("
+           << std::to_string((uintptr_t) huge_buf) << ") failed. ";
+      throw std::runtime_error(xmsg.str());
+    }
+
+    /* Find the SHM region that was used to allocate \p huge_buf */
+    for (shm_region_t shm_region : shm_list) {
+      void *hi = (void *)((char*)shm_region.alloc_buf + shm_region.alloc_size);
+
+      if (huge_buf >= shm_region.alloc_buf && huge_buf < hi) {
+        /* This is the SHM region. Find the number of SHM regions to free. */
+        size_t hugepage_index =
+            ((size_t)huge_buf - (size_t)shm_region.alloc_buf) / kHugepageSize;
+        size_t nb_contig = shm_region.nb_contig_vec[hugepage_index];
+        assert(nb_contig > 0);
+
+        /* Mark the hugepages as free */
+        for (size_t i = 0; i < nb_contig; i++) {
+          assert(shm_region.free_hugepage_vec[hugepage_index + i] == false);
+          shm_region.free_hugepage_vec[hugepage_index + i] = true;
+        }
+
+        shm_region.nb_contig_vec[hugepage_index] = 0;
+        return;
+      }
+    }
+
+    /* We should never get here */
+    std::ostringstream xmsg;
+    xmsg << "eRPC HugeAllocator: free_huge("
+         << std::to_string((uintptr_t) huge_buf) << ") failed. ";
+    throw std::runtime_error(xmsg.str());
   }
 
   /// Return the total amount of memory reserved as hugepages.
@@ -199,14 +261,61 @@ class HugeAllocator {
   }
 
  private:
-  /// Remove num_hugepages from the beginning of this SHM region.
-  inline void pop_hugepages(shm_region_t &shm_region, size_t num_hugepages) {
-    assert(shm_region.free_hugepages >= num_hugepages);
-    shm_region.cur_buf =
-        (void *)((char *)shm_region.cur_buf + (num_hugepages * kHugepageSize));
+  /**
+   * @brief Try to allocate \p num_hugepages contiguous huge pages from \p
+   * shm_region. This uses the free hugepages bitvector in the SHM region.
+   * 
+   * @return The address of the allocated buffer if allocation succeeds. NULL
+   * if allocation is not possible.
+   */
+  inline void* alloc_contiguous(shm_region_t &shm_region,
+                                size_t num_hugepages) {
+    if (shm_region.free_hugepages < num_hugepages) {
+      return nullptr;
+    }
 
-    shm_region.free_hugepages -= num_hugepages;
-    tot_free_hugepages -= num_hugepages;
+    // Try to find a contiguous chunk of \p num_hugepages hugepages
+    size_t start = 0; /* The start index of the contiguous chunk */
+
+    while (start <= shm_region.alloc_hugepages - num_hugepages) {
+      bool start_valid = true;
+      size_t end = start;
+      /* Check if pages [start, ..., start + num_hugepages) are all free */
+      for (; end < start + num_hugepages; end++) {
+        if (shm_region.free_hugepage_vec.at(end) == false) {
+          start_valid = false;
+          break;
+        }
+      }
+
+      if (start_valid) {
+        assert(end == start + num_hugepages - 1);
+        assert(shm_region.free_hugepage_vec.at(end) == true);
+        break;
+      } else {
+        start = end + 1;
+      }
+    }
+
+    if (start == shm_region.alloc_hugepages) {
+      /* We failed to find a valid chunk */
+      return nullptr;
+    } else {
+      /* We have a valid chunk. Mark the hugepages in this chunk not-free */
+      for (size_t i = 0; i < num_hugepages; i++) {
+        shm_region.free_hugepage_vec.at(start + i) = false;
+      }
+
+      /* Record the allocation size */
+      shm_region.nb_contig_vec.at(start) = num_hugepages;
+
+      shm_region.free_hugepages -= num_hugepages;
+      tot_free_hugepages -= num_hugepages;
+
+      void *ret_buf =
+          (void *)((char *)shm_region.alloc_buf + start * kHugepageSize);
+      return ret_buf;
+    }
   }
 
   /**
@@ -238,7 +347,7 @@ class HugeAllocator {
       if (shm_id == -1) {
         switch (errno) {
           case EEXIST:
-            /* @shm_key already exists. Try again. */
+            /* \p shm_key already exists. Try again. */
             break;
 
           case EACCES:
@@ -264,29 +373,25 @@ class HugeAllocator {
             throw std::runtime_error(xmsg.str());
         }
       } else {
-        /* shm_key worked. Break out of the while loop */
+        /* \p shm_key worked. Break out of the while loop */
         break;
       }
     }
 
     void *shm_buf = shmat(shm_id, nullptr, 0);
     if (shm_buf == nullptr) {
-      fprintf(stderr,
-              "eRPC HugeAllocator: SHM malloc error: shmat() failed "
-              "for key %d\n",
-              shm_key);
-      exit(-1);
+      xmsg << "eRPC HugeAllocator: SHM malloc error: shmat() failed for key "
+           << std::to_string(shm_key);
+      throw std::runtime_error(xmsg.str());
     }
 
     /* Bind the buffer to the NUMA node */
     const unsigned long nodemask = (1ul << (unsigned long)numa_node);
     long ret = mbind(shm_buf, size, MPOL_BIND, &nodemask, 32, 0);
     if (ret != 0) {
-      fprintf(stderr,
-              "eRPC HugeAllocator: SHM malloc error. mbind() failed "
-              "for key %d\n",
-              shm_key);
-      exit(-1);
+      xmsg << "eRPC HugeAllocator: SHM malloc error. mbind() failed for key "
+           << shm_key;
+      throw std::runtime_error(xmsg.str());
     }
 
     /* If we are here, the allocation succeeded. Record for deallocation. */
@@ -299,7 +404,7 @@ class HugeAllocator {
     return true;
   }
 
-  void delete_shm(int shm_key, void *shm_buf) {
+  void delete_shm(int shm_key, const void *shm_buf) {
     int shmid = shmget(shm_key, 0, 0);
     if (shmid == -1) {
       switch (errno) {
