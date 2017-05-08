2e0904f74a2ef2b743108ca257a744d10562594f
Anuj Kalia <akalia@cs.cmu.edu>
2017-05-08 13:06:35 -0600

    Save work

--- src/rpc_impl/rpc_pkt_loss.cc
+++ src/rpc_impl/rpc_pkt_loss.cc
@@ -20,21 +20,18 @@ void Rpc<TTr>::pkt_loss_scan_reqs_st() {
     for (SSlot &sslot : session->sslot_arr) {
       // Ignore sslots that don't have a request with an incomplete response
       if (sslot.tx_msgbuf == nullptr) continue;
-
-      // If we're here, sslot has an active request and it isn't recovering
       assert(sslot.tx_msgbuf->get_req_num() == sslot.cur_req_num);
 
       size_t cycles_since_enqueue = rdtsc() - sslot.client_info.enqueue_req_ts;
       size_t ms_since_enqueue = to_msec(cycles_since_enqueue, nexus->freq_ghz);
+      if (ms_since_enqueue < kPktLossTimeoutMs) continue;
 
-      if (ms_since_enqueue >= kPktLossTimeoutMs) {
-        char issue_msg[kMaxIssueMsgLen];  // The basic issue message
-        sprintf(issue_msg,
-                "eRPC Rpc %u: Packet loss suspected for session %u, "
-                "req num %zu. Issue",
-                rpc_id, session->local_session_num,
-                sslot.tx_msgbuf->get_req_num());
-      }
+      // If we're here, we suspect a packet loss for this request
+      char issue_msg[kMaxIssueMsgLen];  // The basic issue message
+      sprintf(
+          issue_msg,
+          "eRPC Rpc %u: Packet loss suspected for session %u, req %zu. Action",
+          rpc_id, session->local_session_num, sslot.tx_msgbuf->get_req_num());
     }
   }
 }
