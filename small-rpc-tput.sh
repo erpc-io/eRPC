d4ca9f01723a2693ccb7f342c260b5b1ab4e5907
Anuj Kalia <akalia@cs.cmu.edu>
2017-06-23 00:10:27 -0600

    Improve small-rpc-tput.sh code style

--- small-rpc-tput.sh
+++ small-rpc-tput.sh
@@ -1,8 +1,8 @@
-for i in $(ipcs -m | awk '{ print $1; }'); do                                   
-    if [[ $i =~ 0x.* ]]; then                                                   
-        sudo ipcrm -M $i 2>/dev/null                                            
-    fi                                                                          
-done                                                                            
+for i in $(ipcs -m | awk '{ print $1; }'); do
+    if [[ $i =~ 0x.* ]]; then
+        sudo ipcrm -M $i 2>/dev/null
+    fi
+done
 
 (cd ~/libmlx4-1.2.1mlnx1/ && ./update_driver.sh)
 sudo ./build/small_rpc_tput $(cat apps/small_rpc_tput/config) --machine_id $1
