bf1b6f6f0cd5803d01427edff5062828ee8682bd
Anuj Kalia <akalia@cs.cmu.edu>
2017-07-06 01:05:33 -0600

    Update test_req_in_req_func to use new request-issuing API.

--- tests/test_req_in_req_func.cc
+++ tests/test_req_in_req_func.cc
@@ -10,6 +10,7 @@
 // should run in the background.
 bool primary_bg, backup_bg;
 
+static constexpr uint8_t kAppDataByte = 10;
 static constexpr size_t kAppNumReqs = 33;
 static_assert(kAppNumReqs > Session::kSessionReqWindow, "");
 
@@ -49,7 +50,7 @@ union tag_t {
 };
 static_assert(sizeof(tag_t) == sizeof(size_t), "");
 
-/// Per-thread application context
+/// Extended context for client
 class AppContext : public BasicAppContext {
  public:
   FastRand fast_rand;
@@ -71,38 +72,39 @@ void req_handler_cp(ReqHandle *req_handle_cp, void *_context) {
   assert(req_handle_cp != nullptr);
   assert(_context != nullptr);
 
-  auto *context = static_cast<AppContext *>(_context);
+  auto *context = static_cast<BasicAppContext *>(_context);
   assert(!context->is_client);
   ASSERT_EQ(context->rpc->in_background(), primary_bg);
 
+  // This will be freed by eRPC when the request handler returns
   const MsgBuffer *req_msgbuf_cp = req_handle_cp->get_req_msgbuf();
   size_t req_size_cp = req_msgbuf_cp->get_data_size();
 
   test_printf("Primary [Rpc %u]: Received request of length %zu.\n",
               context->rpc->get_rpc_id(), req_size_cp);
 
-  // Record info for the request that we are now sending to server #1
+  // Record info for the request that we are now sending to the backup
   PrimaryReqInfo *srv_req_info =
       new PrimaryReqInfo(req_size_cp, req_handle_cp, context->rpc->get_etid());
 
-  MsgBuffer &req_msgbuf_pb = srv_req_info->req_msgbuf_pb;
-  req_msgbuf_pb = context->rpc->alloc_msg_buffer(req_size_cp);
-  assert(req_msgbuf_pb.buf != nullptr);
+  // Allocate request and response MsgBuffers for the request to the backup
+  srv_req_info->req_msgbuf_pb = context->rpc->alloc_msg_buffer(req_size_cp);
+  assert(srv_req_info->req_msgbuf_pb.buf != nullptr);
 
-  MsgBuffer &resp_msgbuf_pb = srv_req_info->resp_msgbuf_pb;
-  resp_msgbuf_pb = context->rpc->alloc_msg_buffer(req_size_cp);
-  assert(resp_msgbuf_pb.buf != nullptr);
+  srv_req_info->resp_msgbuf_pb = context->rpc->alloc_msg_buffer(req_size_cp);
+  assert(srv_req_info->resp_msgbuf_pb.buf != nullptr);
 
-  // Request to server #1 = client-to-server request + 1
+  // Request to backup = client-to-server request + 1
   for (size_t i = 0; i < req_size_cp; i++) {
-    req_msgbuf_pb.buf[i] = req_msgbuf_cp->buf[i] + 1;
+    srv_req_info->req_msgbuf_pb.buf[i] = req_msgbuf_cp->buf[i] + 1;
   }
 
   tag_t tag(srv_req_info);  // Save the request info pointer in the tag
 
+  // Backup is server thread #1
   int ret = context->rpc->enqueue_request(
-      context->session_num_arr[1], kAppReqTypePB, &req_msgbuf_pb,
-      &resp_msgbuf_pb, primary_cont_func, tag.tag);
+      context->session_num_arr[1], kAppReqTypePB, &srv_req_info->req_msgbuf_pb,
+      &srv_req_info->resp_msgbuf_pb, primary_cont_func, tag.tag);
   _unused(ret);
   assert(ret == 0);
 }
@@ -113,7 +115,7 @@ void req_handler_pb(ReqHandle *req_handle, void *_context) {
   assert(req_handle != nullptr);
   assert(_context != nullptr);
 
-  auto *context = static_cast<AppContext *>(_context);
+  auto *context = static_cast<BasicAppContext *>(_context);
   assert(!context->is_client);
   ASSERT_EQ(context->rpc->in_background(), backup_bg);
 
@@ -142,7 +144,7 @@ void primary_cont_func(RespHandle *resp_handle_pb, void *_context,
   assert(resp_handle_pb != nullptr);
   assert(_context != nullptr);
 
-  auto *context = static_cast<AppContext *>(_context);
+  auto *context = static_cast<BasicAppContext *>(_context);
   assert(!context->is_client);
   ASSERT_EQ(context->rpc->in_background(), primary_bg);
 
@@ -159,13 +161,11 @@ void primary_cont_func(RespHandle *resp_handle_pb, void *_context,
   // Extract the request info
   size_t req_size_cp = srv_req_info->req_size_cp;
   ReqHandle *req_handle_cp = srv_req_info->req_handle_cp;
-  MsgBuffer &req_msgbuf_pb = srv_req_info->req_msgbuf_pb;
-
   assert(resp_msgbuf_pb->get_data_size() == req_size_cp);
 
   // Check the response from server #1
   for (size_t i = 0; i < req_size_cp; i++) {
-    assert(req_msgbuf_pb.buf[i] + 1 == resp_msgbuf_pb->buf[i]);
+    assert(srv_req_info->req_msgbuf_pb.buf[i] + 1 == resp_msgbuf_pb->buf[i]);
   }
 
   // eRPC will free dyn_resp_msgbuf
@@ -178,7 +178,8 @@ void primary_cont_func(RespHandle *resp_handle_pb, void *_context,
   }
 
   // Free resources of the server-to-server request
-  context->rpc->free_msg_buffer(req_msgbuf_pb);
+  context->rpc->free_msg_buffer(srv_req_info->req_msgbuf_pb);
+  context->rpc->free_msg_buffer(srv_req_info->resp_msgbuf_pb);
   delete srv_req_info;
 
   // Release the server-server response
@@ -205,19 +206,19 @@ void client_request_helper(AppContext *context, size_t msgbuf_i) {
   context->rpc->resize_msg_buffer(&context->req_msgbuf[msgbuf_i], req_size);
 
   // Fill in all the bytes of the request MsgBuffer with msgbuf_i
-  MsgBuffer &req_msgbuf = context->req_msgbuf[msgbuf_i];
   for (size_t i = 0; i < req_size; i++) {
-    req_msgbuf.buf[i] = static_cast<uint8_t>(msgbuf_i);
+    context->req_msgbuf[msgbuf_i].buf[i] = kAppDataByte;
   }
 
   tag_t tag(static_cast<uint16_t>(context->num_reqs_sent),
             static_cast<uint16_t>(msgbuf_i), static_cast<uint32_t>(req_size));
-  test_printf("Client: Sending request %zu of size %zu\n",
-              context->num_reqs_sent, req_size);
+  test_printf("Client [Rpc %u]: Sending request %zu of size %zu\n",
+              context->rpc->get_rpc_id(), context->num_reqs_sent, req_size);
 
   int ret = context->rpc->enqueue_request(
-      context->session_num_arr[0], kAppReqTypeCP, &req_msgbuf,
-      &context->resp_msgbuf[msgbuf_i], client_cont_func, tag.tag);
+      context->session_num_arr[0], kAppReqTypeCP,
+      &context->req_msgbuf[msgbuf_i], &context->resp_msgbuf[msgbuf_i],
+      client_cont_func, tag.tag);
   _unused(ret);
   assert(ret == 0);
 
@@ -238,20 +239,21 @@ void client_cont_func(RespHandle *resp_handle, void *_context, size_t _tag) {
   size_t req_size = static_cast<size_t>(tag.req_size);
   size_t msgbuf_i = static_cast<size_t>(tag.msgbuf_i);
 
-  test_printf("Client: Received response for req %u, length = %zu.\n",
-              tag.req_i, resp_msgbuf->get_data_size());
+  test_printf("Client [Rpc %u]: Received response for req %u, length = %zu.\n",
+              context->rpc->get_rpc_id(), tag.req_i,
+              resp_msgbuf->get_data_size());
 
   // Check the response
   ASSERT_EQ(resp_msgbuf->get_data_size(), req_size);
   for (size_t i = 0; i < req_size; i++) {
-    ASSERT_EQ(resp_msgbuf->buf[i], static_cast<uint8_t>(msgbuf_i) + 3);
+    ASSERT_EQ(resp_msgbuf->buf[i], kAppDataByte + 3);
   }
 
   context->num_rpc_resps++;
   context->rpc->release_response(resp_handle);
 
   if (context->num_reqs_sent < kAppNumReqs) {
-    client_request_helper(context, static_cast<tag_t>(tag).msgbuf_i);
+    client_request_helper(context, msgbuf_i);
   }
 }
 
@@ -288,6 +290,7 @@ void client_thread(Nexus<IBTransport> *nexus, size_t num_sessions) {
     rpc->destroy_session(context.session_num_arr[i]);
   }
   wait_for_sm_resps_or_timeout(context, num_sessions, nexus->freq_ghz);
+  assert(rpc->num_active_sessions() == 0);
 
   // Free resources
   delete rpc;
