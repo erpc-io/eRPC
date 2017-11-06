8c97bb6f40442016b1f5d9e359a4a618d18b7c98
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-11-06 04:19:21 -0500

    Reorg packages script

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
+#
+# This script sets up a fresh Ubuntu box for eRPC.
+#
+# For best perforamance, Mellanox OFED should be installed by downloading from
+# Mellanox. However, eRPC should work with upstream mlx* packages as well
 
-# Install g++-7
+###
+### Required packages
+###
+
+sudo apt-get -y install cmake libnuma-dev libpapi-dev numactl libgflags-dev libboost-date-time-dev libenet-dev
+
+# GTest is special for some reason
+sudo apt-get -y install libgtest-dev
+(cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/)
+
+###
+### Optional convenience packages
+###
+
+# g++-7
 sudo apt-get update && \
   sudo apt-get install build-essential software-properties-common -y && \
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
   sudo apt-get update && \
   sudo apt-get install g++-7
 
-# Install general packages (not specific to eRPC)
-sudo apt-get install -y cmake htop libnuma-dev libpapi-dev memcached libmemcached-dev numactl clang-format
+# General packages not specific to eRPC
+sudo apt-get install -y htop memcached libmemcached-dev clang-format exuberant-ctags
 
-# Install fuzzy find
+# Fuzzy find
 sudo apt-get install -y silversearcher-ag
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Install Vundle
+# Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Install packages required for eRPC
-sudo apt-get -y install cmake libgflags-dev libboost-date-time-dev libenet-dev libgtest-dev
-
-# Really install gtest
-(cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/)
