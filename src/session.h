8516eb29194cf374e783515604ab37432ded42c2
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-11-10 14:19:23 -0500

    Reorg code so only header files are at top-level

--- src/session.h
+++ src/session.h
@@ -30,8 +30,8 @@ class Session {
  public:
   enum class Role : int { kServer, kClient };
 
-  static constexpr size_t kSessionReqWindow = 8;  ///< *Request* window size
-  static constexpr size_t kSessionCredits = 8;    ///< *Packet* credits
+  static constexpr size_t kSessionReqWindow = 8;  ///< Request window size
+  static constexpr size_t kSessionCredits = 8;    ///< Packet credits
 
   /// Controls printing of credit exhausted alerts
   static constexpr size_t kCreditsExhaustedWarnLim = 1000000000;
@@ -41,7 +41,41 @@ class Session {
   static_assert(is_power_of_two(kSessionReqWindow), "");
 
  private:
-  Session(Role role, SessionState state);
+  Session(Role role, SessionState state) : role(role), state(state) {
+    // A session may be created by the client in kConnectInProgress state, or
+    // by the server in kConnected statet.
+    if (is_client()) {
+      assert(state == SessionState::kConnectInProgress);
+      remote_routing_info = &server.routing_info;
+    } else {
+      assert(state == SessionState::kConnected);
+      remote_routing_info = &client.routing_info;
+    }
+
+    // Arrange the free slot vector so that slots are popped in order
+    for (size_t i = 0; i < kSessionReqWindow; i++) {
+      // Initialize session slot with index = sslot_i
+      size_t sslot_i = (kSessionReqWindow - 1 - i);
+      SSlot &sslot = sslot_arr[sslot_i];
+
+      // This buries all MsgBuffers
+      memset(static_cast<void *>(&sslot), 0, sizeof(SSlot));
+
+      sslot.prealloc_used = true;  // There's no user-allocated memory to free
+      sslot.session = this;
+      sslot.is_client = this->is_client();
+      sslot.index = sslot_i;
+      sslot.cur_req_num = sslot_i;  // 1st req num = (+kSessionReqWindow)
+
+      if (sslot.is_client) {
+        sslot.client_info.cont_etid = kInvalidBgETid;  // Continuations in fg
+      } else {
+        sslot.server_info.req_type = kInvalidReqType;
+      }
+
+      client_info.sslot_free_vec.push_back(sslot_i);
+    }
+  }
 
   /// Session resources are freed in Rpc::bury_session(), so this is empty
   ~Session() {}
