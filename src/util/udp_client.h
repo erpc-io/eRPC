cd9a54d5919bb2942a9a0663fc6a58b548c49f90
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-11-12 02:58:54 -0500

    Add UDP client and server

---
+++ src/util/udp_client.h
@@ -0,0 +1,65 @@
+#ifndef ERPC_UDP_CLIENT_H
+#define ERPC_UDP_CLIENT_H
+
+#include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <map>
+#include <stdexcept>
+
+namespace ERpc {
+
+/// Basic UDP client class that supports sending messages and caches remote
+/// addrinfo mappings
+class UDPClient {
+ public:
+  UDPClient(uint16_t global_udp_port) : global_udp_port(global_udp_port) {
+    snprintf(_port_str, sizeof(_port_str), "%u", global_udp_port);
+
+    sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock_fd == -1) {
+      throw std::runtime_error("UDPClient: Failed to create local socket.");
+    }
+  }
+
+  ~UDPClient() {
+    for (auto kv : addrinfo_map) freeaddrinfo(kv.second);
+    close(sock_fd);
+  }
+
+  ssize_t send(const std::string remote_hostname, const char *msg,
+               size_t size) {
+    struct addrinfo *remote_addrinfo = nullptr;
+    if (addrinfo_map.count(remote_hostname) != 0) {
+      remote_addrinfo = addrinfo_map.at(remote_hostname);
+    } else {
+      struct addrinfo hints;
+      memset(&hints, 0, sizeof(hints));
+      hints.ai_family = AF_UNSPEC;
+      hints.ai_socktype = SOCK_DGRAM;
+      hints.ai_protocol = IPPROTO_UDP;
+
+      int r = getaddrinfo(remote_hostname.c_str(), _port_str, &hints,
+                          &remote_addrinfo);
+      if (r != 0 || remote_addrinfo == nullptr) {
+        throw std::runtime_error("UDPClient: Failed to resolve remote.");
+      }
+
+      addrinfo_map[remote_hostname] = remote_addrinfo;
+    }
+
+    return sendto(sock_fd, msg, size, 0, remote_addrinfo->ai_addr,
+                  remote_addrinfo->ai_addrlen);
+  }
+
+ private:
+  const uint16_t global_udp_port;
+  char _port_str[16];
+  int sock_fd;
+  std::map<std::string, struct addrinfo *> addrinfo_map;
+};
+
+}  // End ERpc
+
+#endif  // UDP_CLIENT_H
---
+++ src/util/udp_server.h
@@ -0,0 +1,43 @@
+#ifndef ERPC_UDP_SERVER_H
+#define ERPC_UDP_SERVER_H
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdexcept>
+
+namespace ERpc {
+
+/// Basic UDP server class that supports receiving messages
+class UDPServer {
+ public:
+  UDPServer(uint16_t global_udp_port) : global_udp_port(global_udp_port) {
+    sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock_fd == -1) {
+      throw std::runtime_error("UDPServer: Failed to create local socket.");
+    }
+
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(static_cast<unsigned short>(global_udp_port));
+
+    int r = bind(sock_fd, reinterpret_cast<struct sockaddr *>(&serveraddr),
+                 sizeof(serveraddr));
+    if (r != 0) throw std::runtime_error("UDPServer: Failed to bind socket.");
+  }
+
+  ~UDPServer() { close(sock_fd); }
+
+  int recv_blocking(char *msg, size_t max_size) {
+    return recv(sock_fd, msg, max_size, 0);
+  }
+
+ private:
+  const uint16_t global_udp_port;
+  int sock_fd;
+};
+
+}  // End ERpc
+
+#endif  // ERPC_UDP_SERVER_H
