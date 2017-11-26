7cb6b4f2a3deca817b5cd32f13a5f830c9ff9189
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-11-26 17:13:16 -0500

    Save work

--- tests/system_tests/rpc_rx_test.cc
+++ tests/system_tests/rpc_rx_test.cc
@@ -55,24 +55,43 @@ TEST_F(RpcRxTest, process_small_req_st) {
   pkthdr_0->msg_size = kTestSmallReqSize;
   pkthdr_0->dest_session_num = server.session_num;
   pkthdr_0->pkt_type = kPktTypeReq;
-
-  // Process an in-order request.
-  // Response handler is called and response is sent.
   pkthdr_0->pkt_num = 0;
   pkthdr_0->req_num = Session::kSessionReqWindow;
+
+  // Receive an in-order small request packet.
+  // Response handler is called and response is sent.
   rpc->process_small_req_st(&srv_session->sslot_arr[0],
                             reinterpret_cast<uint8_t *>(pkthdr_0));
   ASSERT_EQ(test_context.num_req_handler_calls, 1);
   ASSERT_EQ(rpc->testing.pkthdr_tx_queue.pop().pkt_type, PktType::kPktTypeResp);
   test_context.num_req_handler_calls = 0;  // Restore
 
-  // Process the same request again.
+  // Receive the same request packet again.
   // Request handler is not called. Response is re-sent, and TX queue flushed.
   rpc->process_small_req_st(&srv_session->sslot_arr[0],
                             reinterpret_cast<uint8_t *>(pkthdr_0));
   ASSERT_EQ(test_context.num_req_handler_calls, 0);
   ASSERT_EQ(rpc->testing.pkthdr_tx_queue.pop().pkt_type, PktType::kPktTypeResp);
   ASSERT_EQ(rpc->transport->testing.tx_flush_count, 1);
+
+  // Advance the sslot's cur_req_num, and receive the request packet again.
+  // Request handler is not called and response is not re-sent.
+  rpc->session_vec[0]->sslot_arr[0].cur_req_num += Session::kSessionReqWindow;
+  rpc->process_small_req_st(&srv_session->sslot_arr[0],
+                            reinterpret_cast<uint8_t *>(pkthdr_0));
+  ASSERT_EQ(test_context.num_req_handler_calls, 0);
+  ASSERT_EQ(rpc->testing.pkthdr_tx_queue.size(), 0);
+  // Restore
+  rpc->session_vec[0]->sslot_arr[0].cur_req_num -= Session::kSessionReqWindow;
+
+  // Mark the response as unavailable, and receive the request packet again.
+  // Request handler is not called and response is not re-sent.
+  MsgBuffer *tx_msgbuf_save = rpc->session_vec[0]->sslot_arr[0].tx_msgbuf;
+  rpc->session_vec[0]->sslot_arr[0].tx_msgbuf = nullptr;
+  rpc->process_small_req_st(&srv_session->sslot_arr[0],
+                            reinterpret_cast<uint8_t *>(pkthdr_0));
+  ASSERT_EQ(test_context.num_req_handler_calls, 0);
+  rpc->session_vec[0]->sslot_arr[0].tx_msgbuf = tx_msgbuf_save;  // Restore
 }
 
 }  // End erpc
