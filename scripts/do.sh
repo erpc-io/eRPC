0a37206bac73eb20f386464483ab386c091c462c
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-25 20:48:02 -0500

    Support two eRPC processes per machine in do.sh

--- scripts/do.sh
+++ scripts/do.sh
@@ -2,6 +2,7 @@
 # Run an eRPC app on this machine
 source $(dirname $0)/utils.sh
 check_env "autorun_app"
+dual_process=1
 
 drop_shm
 
@@ -24,28 +25,40 @@ else
 fi
 
 # Check arguments
-if [ "$#" -gt 2 ]; then
-  blue "Illegal number of arguments. Usage: do.sh <process_id>, or do.sh gdb <process_id>"
+if [ "$#" -gt 2 ] || [ "$#" -eq 0 ]; then
+  blue "Illegal args. Usage: do.sh <process_id>, or do.sh gdb <process_id>"
 	exit
 fi
 
-if [ "$#" -eq 0 ]; then
-  blue "Illegal number of arguments. Usage: do.sh <process_id>, or do.sh gdb <process_id>"
-	exit
-fi
-
-# Check for non-gdb mode
+# Non-GDB mode
 if [ "$#" -eq 1 ]; then
-  blue "do.sh: machine ID = $1"
-  sudo ./build/$autorun_app $(cat apps/$autorun_app/config) --process_id $1
+  if [ "$dual_process" -eq 0 ]; then
+    # Compute eRPC process IDs
+    epid_0=`expr 2 \* $1`
+    epid_1=`expr 2 \* $1 + 1`
+    blue "do.sh: Launching process $epid_0 (NUMA 0) and $epid_1 (NUMA 1)"
+
+    sudo numactl --cpunodebind=0 --membind=0 ./build/$autorun_app \
+      $(cat apps/$autorun_app/config) --process_id $epid_0 --numa_node 0
+
+    sudo numactl --cpunodebind=1 --membind=1 ./build/$autorun_app \
+      $(cat apps/$autorun_app/config) --process_id $epid_1 --numa_node 1
+  else
+    epid_0=$1
+    blue "do.sh: Launching process $epid_0 (NUMA 0)"
+
+    sudo numactl --cpunodebind=0 --membind=0 ./build/$autorun_app \
+      $(cat apps/$autorun_app/config) --process_id $epid_0 --numa_node 0
+  fi
 fi
 
-# Check for gdb mode
+# GDB mode
 if [ "$#" -eq 2 ]; then
-  if [ "$1" == "gdb" ]; then
-    blue "do.sh: machine ID = $1. Launching gdb."
-    sudo gdb -ex run --args ./build/$autorun_app $(cat apps/$autorun_app/config) --process_id $2
-  else
-    blue "do.sh: Invalid parameter 1. Expected = gdb."
+  if [ "$dual_process" -eq 1 ]; then
+    blue "do.sh: Cannot run GDB in dual-process mode. Exiting. "
+    exit
   fi
+
+  blue "do.sh: machine ID = $1. Launching GDB."
+  sudo gdb -ex run --args ./build/$autorun_app $(cat apps/$autorun_app/config) --process_id $2
 fi
