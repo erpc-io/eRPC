9c0929ddef61f70a5930ce6819b2bd11cb7b13d0
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-29 19:46:57 -0500

    Add bc to packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -10,7 +10,8 @@
 ###
 
 sudo apt-get update
-sudo apt-get -y install cmake libnuma-dev libpapi-dev numactl libgflags-dev libboost-date-time-dev
+sudo apt-get -y install cmake libnuma-dev libpapi-dev numactl bc \
+  libgflags-dev libboost-date-time-dev
 
 # GTest is special for some reason
 sudo apt-get -y install libgtest-dev
