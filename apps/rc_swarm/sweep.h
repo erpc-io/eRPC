3e974fd9cc7296635f7b7378c3704767cfc6979a
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-01-03 19:28:01 -0500

    Remove rc_swarm code from eRPC. It's in rdma_bench.

--- apps/rc_swarm/sweep.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-static constexpr size_t kAppNumMachines = 3;
-static constexpr size_t kAppVMsPerMachine = 1;
-static constexpr size_t kAppNumWorkers = 42;
-static constexpr size_t kAppRDMASize = 64;
-static constexpr size_t kAppWindowSize = 32;
-static constexpr bool kAppAllsig = false;
-static constexpr size_t kAppUnsigBatch = 32;
