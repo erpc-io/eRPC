aa0656f064889f9aafd77d68e788354809cd17ef
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-01-09 21:52:32 -0500

    Save

--- tests/util_tests/timely_test.cc
+++ tests/util_tests/timely_test.cc
@@ -2,13 +2,15 @@
 using namespace erpc;
 
 int main() {
-  Timely timely(measure_rdtsc_freq());
+  double freq_ghz = measure_rdtsc_freq();
+  Timely timely(freq_ghz);
   std::vector<double> sample_seq = {12,   13,   20, 20, 40, 2000, 2000,
                                     2000, 2000, 12, 20, 60, 20,   30,
                                     55,   60,   20, 20, 20, 20,   20};
 
   for (double rtt : sample_seq) {
     timely.update_rate(rtt);
+    nano_sleep(1000, freq_ghz);  // Update every one microsecond
     printf("RTT = %.2f, tput = %.2f Gbps, avg_rtt_diff = %.2f us\n", rtt,
            Timely::rate_to_gbps(timely.rate), timely.get_avg_rtt_diff());
   }
