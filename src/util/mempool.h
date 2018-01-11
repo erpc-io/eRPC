a673dc996de93b5d7bef09a7d96860f95cf36aeb
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-01-10 21:04:37 -0500

    Implement hugepage-backed mempool

--- src/util/mempool.h
+++ src/util/mempool.h
@@ -6,24 +6,24 @@
 
 namespace erpc {
 
-/// A hugepage-backed pool for objects of type T
+/// A hugepage-backed pool for objects of type T. Objects are not registered
+/// with the NIC.
 template <class T>
 class MemPool {
-  size_t num_to_alloc = 1024;
+  size_t num_to_alloc = MB(2) / sizeof(T);  // Start with 2 MB
   HugeAlloc *huge_alloc;
   std::vector<T *> pool;
 
   void extend_pool() {
     size_t alloc_sz = sizeof(T) * num_to_alloc;
-    alloc_sz = std::max(alloc_sz, HugeAlloc::kMaxClassSize);
 
-    Buffer b = huge_alloc->alloc(alloc_sz);
+    // alloc_raw()'s result is leaked
+    Buffer b = huge_alloc->alloc_raw(alloc_sz, DoRegister::kFalse);
     rt_assert(b.buf != nullptr, "Hugepage allocation failed");
 
     for (size_t i = 0; i < num_to_alloc; i++) {
-      pool.push_back(&backing_ptr[i]);
+      pool.push_back(&b.buf[sizeof(T) * i]);
     }
-    backing_ptr_vec.push_back(backing_ptr);
     num_to_alloc *= 2;
   }
 
@@ -38,9 +38,8 @@ class MemPool {
 
   MemPool(HugeAlloc *huge_alloc) : huge_alloc(huge_alloc) {}
 
-  ~MemPool() {
-    for (Buffer &b : backing_buffer_vec) huge_alloc->free_buf(b);
-  }
+  /// Cleanup is done when owner deletes huge_alloc
+  ~MemPool() {}
 };
 }
 
