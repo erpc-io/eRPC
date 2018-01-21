94fd86c00ad3823d06ae4f2c92bbec25e063dc6e
Anuj Kalia <akalia@cs.cmu.edu>
2018-01-20 18:34:09 -0700

    Update Timely test

--- tests/util_tests/timely_test.cc
+++ tests/util_tests/timely_test.cc
@@ -1,21 +1,29 @@
 #include "cc/timely.h"
 using namespace erpc;
 
-int main() {
+void test(size_t mean_rtt, size_t random_add_rtt) {
   double freq_ghz = measure_rdtsc_freq();
   Timely timely(freq_ghz);
 
   std::vector<double> sample_us;
   for (size_t i = 0; i < 2000; i++) {
     // double rtt_sample = 500 + (0.2 * i);
-    double rtt_sample = 500 + rand() % 1;
+    double rtt_sample = mean_rtt + static_cast<size_t>(rand()) % random_add_rtt;
     sample_us.push_back(rtt_sample);
   }
 
   for (double rtt_us : sample_us) {
     timely.update_rate(rdtsc(), us_to_cycles(rtt_us, freq_ghz));
     nano_sleep(1000, freq_ghz);  // Update every one microsecond
-    printf("RTT = %.2f us, tput = %.2f Gbps, avg_rtt_diff = %.2f us\n", rtt_us,
-           timely.get_rate_gbps(), timely.get_avg_rtt_diff());
+  }
+
+  printf("mean %zu us, random %zu us, tput %.2f Gbps\n", mean_rtt,
+         random_add_rtt, timely.get_rate_gbps());
+}
+
+int main() {
+  size_t random_add_rtt = 5;
+  for (size_t iter = 0; iter < 20; iter++) {
+    test(Timely::kTLow + iter * 10, random_add_rtt);
   }
 }
