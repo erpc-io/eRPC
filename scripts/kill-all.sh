05ba942d2235602ac2b48fd2424d34ddeae53f89
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-01-29 18:09:58 -0500

    Improve kill-all script

--- scripts/kill-all.sh
+++ scripts/kill-all.sh
@@ -4,14 +4,14 @@ source $(dirname $0)/autorun_parse.sh
 
 blue "Killing $autorun_app everywhere"
 
-for i in `seq 1 $autorun_num_processes`; do
+for ((i = 0; i < $autorun_num_processes; i++)); do
   name=${autorun_name_list[$i]}
 	ssh -oStrictHostKeyChecking=no $name "sudo killall $autorun_app 1>/dev/null 2>/dev/null" &
 done
 wait
 
 blue "Printing $autorun_app processes that are still running..."
-for i in `seq 1 $autorun_num_processes`; do
+for ((i = 0; i < $autorun_num_processes; i++)); do
   (
 	ret=`ssh -oStrictHostKeyChecking=no ${autorun_name_list[$i]} "pgrep -x $autorun_app"`
   if [ -n "$ret" ]; then
