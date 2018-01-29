dc90e4cde996340c2b7561261818bb94a6fb837f
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-01-29 01:00:35 -0500

    Improve RDMA write bw scripts

--- apps/rdma_write_bw/fawn/client.sh
+++ apps/rdma_write_bw/fawn/client.sh
@@ -3,11 +3,13 @@ killall ib_write_bw
 
 # Check arguments
 if [ "$#" -ne 1 ]; then
-  echo "Illegal args. Usage: client.sh <size>"
+  echo "Illegal args. Usage: client.sh <size in KB>"
 	exit
 fi
+
+size=`expr 1024 \* $1`
 
 # We need tx-depth=1 to have only one WRITE in flight
 ib_write_bw --gid-index=0 --port=3185 --ib-dev=mlx5_0 --ib-port=1 \
   --tx-depth=1 --report_gbits --run_infinitely --duration=1 \
-  --size=$1 fawn-pluto0
+  --size=$size fawn-pluto0
