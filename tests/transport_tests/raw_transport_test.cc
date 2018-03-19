4fdbc963362149c61b9e4984cdef4fe236b305de
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-03-19 17:17:08 -0600

    Clean up unused stuff from raw_transport_test

--- tests/transport_tests/raw_transport_test.cc
+++ tests/transport_tests/raw_transport_test.cc
@@ -36,19 +36,6 @@ class RawTransportTest : public ::testing::Test {
     // Initialize TX msgbufs. All packets are sent to self.
     transport->fill_local_routing_info(&self_ri);
     transport->resolve_remote_routing_info(&self_ri);  // Treat self as remote
-
-    for (size_t i = 0; i < RawTransport::kPostlist; i++) {
-      Buffer buf = huge_alloc->alloc(RawTransport::kMTU);
-      assert(buf.buf != nullptr);
-
-      tx_msgbuf[i] = MsgBuffer(buf, RawTransport::kMTU - sizeof(pkthdr_t), 1);
-
-      tx_burst_arr[i].routing_info = &self_ri;
-      tx_burst_arr[i].msg_buffer = &tx_msgbuf[i];
-      tx_burst_arr[i].pkt_index = 0;
-      tx_burst_arr[i].tx_ts = &tx_ts[i];
-      tx_burst_arr[i].drop = false;
-    }
   }
 
   ~RawTransportTest() {
@@ -61,9 +48,6 @@ class RawTransportTest : public ::testing::Test {
   uint8_t* rx_ring[RawTransport::kNumRxRingEntries];
 
   Transport::RoutingInfo self_ri;
-  Transport::tx_burst_item_t tx_burst_arr[RawTransport::kPostlist];
-  MsgBuffer tx_msgbuf[RawTransport::kPostlist];
-  size_t tx_ts[RawTransport::kPostlist];
 
   // Create a ready-to-send packet with destination = self, and with space for
   // \p data_size user data bytes
