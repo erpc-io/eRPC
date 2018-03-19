45be40dcfe39b5fd2b3a6cf222ffd52942f4a27f
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-03-19 16:08:33 -0400

    Save work on raw transport test

--- tests/transport_tests/raw_transport_test.cc
+++ tests/transport_tests/raw_transport_test.cc
@@ -17,12 +17,30 @@ class RawTransportTest : public ::testing::Test {
       return;
     }
 
+    // Initalize the transport
     transport = new RawTransport(kTestRpcId, kTestPhyPort, kTestNumaNode);
 
     huge_alloc = new HugeAlloc(MB(32), kTestNumaNode, transport->reg_mr_func,
                                transport->dereg_mr_func);
 
     transport->init_hugepage_structures(huge_alloc, rx_ring);
+
+    // Initialize TX msgbufs. All packets are sent to self.
+    transport->fill_local_routing_info(&self_ri);
+    transport->resolve_remote_routing_info(&self_ri);
+
+    for (size_t i = 0; i < RawTransport::kPostlist; i++) {
+      Buffer buf = huge_alloc->alloc(RawTransport::kMTU);
+      assert(buf.buf != nullptr);
+
+      tx_msgbuf[i] = MsgBuffer(buf, RawTransport::kMTU - sizeof(pkthdr_t), 1);
+
+      tx_burst_arr[i].routing_info = &self_ri;
+      tx_burst_arr[i].msg_buffer = &tx_msgbuf[i];
+      tx_burst_arr[i].pkt_index = 0;
+      tx_burst_arr[i].tx_ts = &tx_ts[i];
+      tx_burst_arr[i].drop = false;
+    }
   }
 
   ~RawTransportTest() {
@@ -30,9 +48,14 @@ class RawTransportTest : public ::testing::Test {
     delete transport;
   }
 
-  uint8_t *rx_ring[RawTransport::kNumRxRingEntries];
   HugeAlloc *huge_alloc;
   RawTransport *transport;
+  uint8_t *rx_ring[RawTransport::kNumRxRingEntries];
+
+  Transport::RoutingInfo self_ri;
+  Transport::tx_burst_item_t tx_burst_arr[RawTransport::kPostlist];
+  MsgBuffer tx_msgbuf[RawTransport::kPostlist];
+  size_t tx_ts[RawTransport::kPostlist];
 };
 
 // Test if we we can create and destroy a transport instance
