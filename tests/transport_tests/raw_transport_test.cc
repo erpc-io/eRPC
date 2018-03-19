34a342bd7816b2a0fa597544e7089e76c4c79d4a
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-03-19 17:40:20 -0600

    Use two transport endpoints for raw transport test. Loopback doesn't work for some reason.

--- tests/transport_tests/raw_transport_test.cc
+++ tests/transport_tests/raw_transport_test.cc
@@ -1,3 +1,6 @@
+// Ideally these tests would use a loopback, but loopback doesn't work for some
+// reason.
+
 #include <gtest/gtest.h>
 
 #define private public  // XXX: Do we need this
@@ -6,11 +9,18 @@
 
 namespace erpc {
 static constexpr size_t kTestPhyPort = 0;
-static constexpr size_t kTestRpcId = 100;
+static constexpr size_t kTestRpcIdClient = 100;
+static constexpr size_t kTestRpcIdServer = 200;
 static constexpr size_t kTestNumaNode = 0;
 
 static constexpr size_t kTestSmallMsgSize = 32;   // Data in small messages
 static constexpr size_t kTestLargeMsgSize = 900;  // Data in large messages
+
+struct transport_info_t {
+  HugeAlloc* huge_alloc;
+  RawTransport* transport;
+  uint8_t* rx_ring[RawTransport::kNumRxRingEntries];
+};
 
 class RawTransportTest : public ::testing::Test {
  public:
@@ -25,41 +35,51 @@ class RawTransportTest : public ::testing::Test {
       return;
     }
 
-    // Initalize the transport
-    transport = new RawTransport(kTestRpcId, kTestPhyPort, kTestNumaNode);
+    // Initalize client transport
+    clt_ttr.transport =
+        new RawTransport(kTestRpcIdClient, kTestPhyPort, kTestNumaNode);
+    clt_ttr.huge_alloc =
+        new HugeAlloc(MB(32), kTestNumaNode, clt_ttr.transport->reg_mr_func,
+                      clt_ttr.transport->dereg_mr_func);
+    clt_ttr.transport->init_hugepage_structures(clt_ttr.huge_alloc,
+                                                clt_ttr.rx_ring);
 
-    huge_alloc = new HugeAlloc(MB(32), kTestNumaNode, transport->reg_mr_func,
-                               transport->dereg_mr_func);
+    // Initialize server transport
+    srv_ttr.transport =
+        new RawTransport(kTestRpcIdServer, kTestPhyPort, kTestNumaNode);
+    srv_ttr.huge_alloc =
+        new HugeAlloc(MB(32), kTestNumaNode, srv_ttr.transport->reg_mr_func,
+                      srv_ttr.transport->dereg_mr_func);
+    srv_ttr.transport->init_hugepage_structures(srv_ttr.huge_alloc,
+                                                srv_ttr.rx_ring);
 
-    transport->init_hugepage_structures(huge_alloc, rx_ring);
-
-    // Initialize TX msgbufs. All packets are sent to self.
-    transport->fill_local_routing_info(&self_ri);
-    transport->resolve_remote_routing_info(&self_ri);  // Treat self as remote
+    // Precompute the UDP header for the server
+    srv_ttr.transport->fill_local_routing_info(&srv_ri);
+    clt_ttr.transport->resolve_remote_routing_info(&srv_ri);
   }
 
   ~RawTransportTest() {
-    delete huge_alloc;
-    delete transport;
+    delete clt_ttr.huge_alloc;
+    delete clt_ttr.transport;
+
+    delete srv_ttr.huge_alloc;
+    delete srv_ttr.transport;
   }
 
-  HugeAlloc* huge_alloc;
-  RawTransport* transport;
-  uint8_t* rx_ring[RawTransport::kNumRxRingEntries];
+  transport_info_t srv_ttr, clt_ttr;
+  Transport::RoutingInfo srv_ri;  // We only need the server's routing info
 
-  Transport::RoutingInfo self_ri;
-
-  // Create a ready-to-send packet with destination = self, and with space for
+  // Create a ready-to-send packet from client to server, with space for
   // \p data_size user data bytes
   Buffer create_packet(size_t data_size) {
     size_t pkt_size = kInetHdrsTotSize + data_size;
-    Buffer buffer = huge_alloc->alloc(pkt_size);  // Get a registered buffer
+    Buffer buffer = clt_ttr.huge_alloc->alloc(pkt_size);
     assert(buffer.buf != nullptr);
 
     memset(buffer.buf, 0, pkt_size);
     auto* pkthdr = reinterpret_cast<pkthdr_t*>(buffer.buf);
 
-    memcpy(&pkthdr->headroom[0], &self_ri, sizeof(Transport::RoutingInfo));
+    memcpy(&pkthdr->headroom[0], &srv_ri, sizeof(Transport::RoutingInfo));
 
     auto* ipv4_hdr =
         reinterpret_cast<ipv4_hdr_t*>(&pkthdr->headroom[sizeof(eth_hdr_t)]);
@@ -95,13 +115,11 @@ TEST_F(RawTransportTest, one_small_tx) {
   send_wr.num_sge = 1;
 
   struct ibv_send_wr* bad_wr;
-  int ret = ibv_post_send(transport->qp, &send_wr, &bad_wr);
+  int ret = ibv_post_send(clt_ttr.transport->qp, &send_wr, &bad_wr);
   ASSERT_EQ(ret, 0);
 
-  poll_cq_one_helper(transport->send_cq);
-  poll_cq_one_helper(transport->recv_cq);
-
-  huge_alloc->free_buf(buffer);
+  poll_cq_one_helper(clt_ttr.transport->send_cq);
+  poll_cq_one_helper(srv_ttr.transport->recv_cq);
 }
 
 }  // End erpc
