f553033975f9805df7e4d41857a834d3ece9badf
anujkaliaiitd <anujkaliaiitd@gmail.com>
2018-04-12 19:00:35 -0400

    Add H-Store licence to logger.h

--- src/util/logger.h
+++ src/util/logger.h
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by H-Store Project                                 *
+ *   Brown University                                                      *
+ *   Massachusetts Institute of Technology                                 *
+ *   Yale University                                                       *
+ *                                                                         *
+ *   This software may be modified and distributed under the terms         *
+ *   of the MIT license.  See the LICENSE file for details.                *
+ *                                                                         *
+ ***************************************************************************/
+
 /**
  * @file logger.h
  * @brief Logging macros that can be optimized out
