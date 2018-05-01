1156ed44c8ff699b9e14fbd1281a9bfcb0d40b85
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-05-01 14:47:47 -0600

    improve rdma write bandwidth scripts for cloudlab

--- apps/rdma_write_bw/cloudlab_xl170/client.sh
+++ apps/rdma_write_bw/cloudlab_xl170/client.sh
@@ -11,5 +11,5 @@ size=`expr 1024 \* $1`
 
 # We need tx-depth=1 to have only one WRITE in flight
 ib_write_bw --gid-index=0 --port=3185 --ib-dev=mlx5_3 --ib-port=1 \
-  --tx-depth=10 --report_gbits --run_infinitely --duration=1 \
-  --size=$size 128.110.154.141
+  --tx-depth=1 --report_gbits --run_infinitely --duration=1 \
+  --size=$size akalianode-1.rdma.ron-pg0.utah.cloudlab.us
