8dea105906eaf07f24099e9062c2bb0f68e7b631
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-05-16 19:10:01 -0600

    minor improvements to scripts

--- scripts/timely_sweep.sh
+++ scripts/timely_sweep.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env bash
 source $(dirname $0)/utils.sh
 
+function echo_and_log() {
+  echo $1
+  echo $1 > sweep_out
+}
+
 # Sweep over Timely's congestion control params
 sweep_dir="/tmp/timely_sweep"
 rm -rf $sweep_dir
 
 timely_sweep_params_h="src/cc/timely_sweep_params.h"
 
-for kPatched in true false; do
-	for kEwmaAlpha in .01 .02 .03 .04 `seq 0.06 0.04 0.85`; do
-		for kBeta in .01 .02 .03 .04 `seq 0.06 0.04 0.85`; do
-			rm $timely_sweep_params_h
+for kPatched in false; do
+	for kEwmaAlpha in .4 .6; do
+    for kBeta in `seq .2 .04 .4`; do
+      rm $timely_sweep_params_h
       touch $timely_sweep_params_h
 
       echo "static constexpr bool kPatched = $kPatched;" >> $timely_sweep_params_h
       echo "static constexpr double kEwmaAlpha = $kEwmaAlpha;" >> $timely_sweep_params_h
       echo "static constexpr double kBeta = $kBeta;" >> $timely_sweep_params_h
       
-      echo "Building for kPatched = $kPatched, kEwmaAlpha = $kEwmaAlpha, kBeta = $kBeta"
+      echo_and_log "Building for kPatched = $kPatched, kEwmaAlpha = $kEwmaAlpha, kBeta = $kBeta"
       make clean
-			make -j 1>/dev/null 2>/dev/null
+      make -j 1>/dev/null 2>/dev/null
 
-      echo "Running"
-			./scripts/run-all.sh 1>/dev/null 2>/dev/null
+      echo_and_log "Running"
+      ./scripts/run-all.sh 1>/dev/null 2>/dev/null
 
-      echo "Fetching output"
+      echo_and_log "Fetching output"
       ./scripts/proc-out.sh | grep -E "Final column tx_gbps|Final column rtt"
 
-      echo "Cleaning up"
+      echo_and_log "Cleaning up"
       ./scripts/kill-all.sh 1>/dev/null 2>/dev/null
 
-      echo ""
-		done
-	done
+      echo_and_log ""
+    done
+  done
 done
