c53c6e253ac301eb32c2679b7141c49951ed8b0f
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-05-28 18:44:29 -0600

    Always install both mlx5 and mlx4 modded drivers

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -31,6 +31,8 @@ for ((i = 0; i < $autorun_num_processes; i++)); do
     source scripts/utils.sh; \
     export autorun_app=$autorun_app; \
     drop_shm; \
+    sudo ./drivers/libmlx5-41mlnx1/update-driver.sh; \
+    sudo ./drivers/libmlx4-41mlnx1/update-driver.sh; \
     sudo nohup numactl --physcpubind $numa_node --membind $numa_node \
     ./build/$autorun_app $app_args --process_id $i --numa_node $numa_node \
     > $out_file 2> $err_file < /dev/null &" &
--- src/tweakme.h
+++ src/tweakme.h
@@ -22,13 +22,13 @@ static constexpr bool kCcRateComp = ENABLE_CC;  ///< Perform rate computation
 static constexpr bool kCcPacing = ENABLE_CC;    ///< Use rate limiter for pacing
 
 /// Sample RDTSC once per RX/TX batch for RTT measurements
-static constexpr bool kCcOptBatchTsc = ENABLE_CC_OPTS;
+static constexpr bool kCcOptBatchTsc = false;
 
 /// Bypass timing wheel if a session is uncongested
-static constexpr bool kCcOptWheelBypass = ENABLE_CC_OPTS;
+static constexpr bool kCcOptWheelBypass = false;
 
 /// Bypass Timely rate update if session is uncongested and RTT is below T_low
-static constexpr bool kCcOptTimelyBypass = ENABLE_CC_OPTS;
+static constexpr bool kCcOptTimelyBypass = false;
 
 static_assert(kCcRTT || !kCcRateComp, "");  // Rate comp => RTT measurement
 
