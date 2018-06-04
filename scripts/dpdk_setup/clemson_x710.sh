3450bcf369c1bc44e443fb7814b66f31fb634ca8
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-06-04 18:49:33 -0400

    add script to initialize dpdk on clemson x710

---
+++ scripts/dpdk_setup/clemson_x710.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+dpdk=~/dpdk
+
+# Load DPDK kernel modules
+sudo modprobe uio
+sudo insmod $dpdk/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+
+# Bind 10 GbE to DPDK
+sudo ifconfig enp24s0f1 down
+sudo $dpdk/usertools/dpdk-devbind.py --bind=igb_uio enp24s0f1
+
+# Create hugepage mount
+sudo mkdir -p /mnt/huge
+grep -s /mnt/huge /proc/mounts > /dev/null
+if [ $? -ne 0 ] ; then
+	sudo mount -t hugetlbfs nodev /mnt/huge
+fi
