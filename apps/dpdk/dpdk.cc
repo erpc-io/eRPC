a53bb1606ea6e4418b6470181d0b271c979eb8f1
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-06-05 17:03:32 -0400

    dpdk wip

--- apps/dpdk/dpdk.cc
+++ apps/dpdk/dpdk.cc
@@ -94,60 +94,10 @@ int main() {
   ret = rte_eth_dev_start(kAppPortId);
   erpc::rt_assert(ret >= 0, "Failed to start port");
 
-  printf("Success\n");
-
-  /*
   // Retrieve the link speed and compute information based on it.
   struct rte_eth_link link;
   rte_eth_link_get(kAppPortId, &link);
-  if (!link.link_status) {
-    throw DriverException(
-        HERE, format("Failed to detect a link on Ethernet port %u",
-  kAppPortId));
-  }
-  if (link.link_speed != ETH_SPEED_NUM_NONE) {
-    // Be conservative about the link speed. We use bandwidth in
-    // QueueEstimator to estimate # bytes outstanding in the NIC's
-    // TX queue. If we overestimate the bandwidth, under high load,
-    // we may keep queueing packets faster than the NIC can consume,
-    // and build up a queue in the TX queue.
-    bandwidthMbps = (uint32_t)(link.link_speed * 0.98);
-  } else {
-    LOG(WARNING,
-        "Can't retrieve network bandwidth from DPDK; "
-        "using default of %d Mbps",
-        bandwidthMbps);
-  }
-  queueEstimator.setBandwidth(bandwidthMbps);
-  maxTransmitQueueSize =
-      (uint32_t)(static_cast<double>(bandwidthMbps) * MAX_DRAIN_TIME / 8000.0);
-  uint32_t maxPacketSize = getMaxPacketSize();
-  if (maxTransmitQueueSize < 2 * maxPacketSize) {
-    // Make sure that we advertise enough space in the transmit queue to
-    // prepare the next packet while the current one is transmitting.
-    maxTransmitQueueSize = 2 * maxPacketSize;
-  }
-
-  // create an in-memory ring, used as a software loopback in order to handle
-  // packets that are addressed to the localhost.
-  loopbackRing = rte_ring_create("dpdk_loopback_ring", 4096, SOCKET_ID_ANY, 0);
-  if (nullptr == loopbackRing) {
-    throw DriverException(HERE, format("Failed to allocate loopback ring: %s",
-                                       rte_strerror(rte_errno)));
-  }
-
-  LOG(NOTICE,
-      "DpdkDriver locator: %s, bandwidth: %d Mbits/sec, "
-      "maxTransmitQueueSize: %u bytes",
-      locatorString.c_str(), bandwidthMbps, maxTransmitQueueSize);
-
-  // DPDK during initialization (rte_eal_init()) pins the running thread
-  // to a single processor. This becomes a problem as the master worker
-  // threads are created after the initialization of the transport, and
-  // thus inherit the (very) restricted affinity to a single core. This
-  // essentially kills performance, as every thread is contenting for a
-  // single core. Revert this, by restoring the affinity to the default
-  // (all cores).
-  Util::clearCpuAffinity();
-  */
+  erpc::rt_assert(link.link_status > 0, "Failed to detect link");
+  erpc::rt_assert(link.link_speed != ETH_SPEED_NUM_NONE, "Failed to get bw");
+  printf("Link bandwidth = %u Mbps\n", link.link_speed);
 }
