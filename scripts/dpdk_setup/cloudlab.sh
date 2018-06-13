b9feb252ecc02b0bd53162ee5214085a87dfb6aa
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-06-13 16:09:41 -0400

    improve cloudlab dpdk setup script

--- scripts/dpdk_setup/cloudlab.sh
+++ scripts/dpdk_setup/cloudlab.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 dpdk=~/dpdk
 
+if [ ! -d "$dpdk" ]; then
+  echo "DPDK directory "$dpdk" does not exist"
+  exit
+fi
+
 # Load DPDK kernel modules
 sudo modprobe uio
+
+if [ ! -f "$dpdk"/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko ]; then
+  echo "igb_uio not found in $dpdk"
+  exit
+fi
+
 sudo insmod "$dpdk"/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
 
 # Bind CloudLab's experimental interface (10.*.*.*) to DPDK
