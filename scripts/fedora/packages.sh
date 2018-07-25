7158f7071722531628894f6d30d7b53912d19a9a
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-07-24 18:08:44 -0700

    add packages.sh for fedora

---
+++ scripts/fedora/packages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# This script sets up a fresh Fedora 27 box for eRPC.
+#
+# For best perforamance, Mellanox OFED should be installed by downloading from
+# Mellanox. However, eRPC should work with upstream mlx* packages as well
+
+###
+### Required packages
+###
+
+sudo dnf -y install gcc-c++ cmake numactl-devel numactl bc gflags-devel
+
+# GTest is special for some reason
+sudo dnf -y install gtest gtest-devel
+#(cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/)
+
+###
+### Optional convenience packages
+###
+
+# Fix vim
+sudo dnf -y upgrade vim-minimal
+sudo dnf -y install vim-enhanced
+
+# General packages not specific to eRPC
+sudo dnf -y install htop memcached memcached-devel git-clang-format \
+  ctags-etags the_silver_searcher sloccount calc
+
+# Fuzzy find configuration
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
