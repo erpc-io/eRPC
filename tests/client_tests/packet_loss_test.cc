d04370707ef868602a50e6b3f81330ffd1e526bf
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-07 19:16:44 -0600

    Reduce packet loss test running time.
    This is done by reducing the packet loss rate, and by using only one
    session. Packet loss handling across different sessions is independent,
    so it's meaningless to test that.

--- tests/client_tests/packet_loss_test.cc
+++ tests/client_tests/packet_loss_test.cc
@@ -21,12 +21,11 @@ class AppContext : public BasicAppContext {
   FastRand fastrand;  ///< Used for picking large message sizes
 };
 
-static constexpr double kPktDropProb = 0.5;
+static constexpr double kPktDropProb = 0.3;
 
 /// Configuration for controlling the test
 size_t config_num_iters;         ///< The number of iterations
-size_t config_num_sessions;      ///< Number of sessions created by client
-size_t config_rpcs_per_session;  ///< Number of Rpcs per session per iteration
+size_t config_num_rpcs;  ///< Number of Rpcs per iteration
 size_t config_num_bg_threads;    ///< Number of background threads
 
 /// The common request handler for all subtests. Copies the request message to
@@ -74,27 +73,23 @@ void cont_func(RespHandle *resp_handle, void *_context, size_t tag) {
   context->rpc->release_response(resp_handle);
 }
 
-/// The generic test function that issues \p config_rpcs_per_session Rpcs
-/// on each of \p config_num_sessions sessions, for multiple iterations.
+/// The generic test function that issues \p config_num_rpcs Rpcs
+/// on the session, for multiple iterations.
 ///
 /// The second \p size_t argument exists only because the client thread function
 /// template in client_tests.h requires it.
 void generic_test_func(Nexus *nexus, size_t) {
   // Create the Rpc and connect the session
   AppContext context;
-  client_connect_sessions(nexus, context, config_num_sessions,
-                          basic_sm_handler);
+  client_connect_sessions(nexus, context, 1, basic_sm_handler);  // 1 session
 
   Rpc<CTransport> *rpc = context.rpc;
   rpc->fault_inject_set_pkt_drop_prob_st(kPktDropProb);
 
-  int *session_num_arr = context.session_num_arr;
-
   // Pre-create MsgBuffers so we can test reuse and resizing
-  size_t tot_reqs_per_iter = config_num_sessions * config_rpcs_per_session;
-  std::vector<MsgBuffer> req_msgbufs(tot_reqs_per_iter);
-  std::vector<MsgBuffer> resp_msgbufs(tot_reqs_per_iter);
-  for (size_t req_i = 0; req_i < tot_reqs_per_iter; req_i++) {
+  std::vector<MsgBuffer> req_msgbufs(config_num_rpcs);
+  std::vector<MsgBuffer> resp_msgbufs(config_num_rpcs);
+  for (size_t req_i = 0; req_i < config_num_rpcs; req_i++) {
     req_msgbufs[req_i] = rpc->alloc_msg_buffer(rpc->get_max_msg_size());
     assert(req_msgbufs[req_i].buf != nullptr);
 
@@ -109,46 +104,39 @@ void generic_test_func(Nexus *nexus, size_t) {
     test_printf("Client: Iteration %zu.\n", iter);
     size_t iter_req_i = 0;  // Request MsgBuffer index in this iteration
 
-    for (size_t sess_i = 0; sess_i < config_num_sessions; sess_i++) {
-      for (size_t w_i = 0; w_i < config_rpcs_per_session; w_i++) {
-        assert(iter_req_i < tot_reqs_per_iter);
-        MsgBuffer &cur_req_msgbuf = req_msgbufs[iter_req_i];
+    for (size_t w_i = 0; w_i < config_num_rpcs; w_i++) {
+      assert(iter_req_i < config_num_rpcs);
+      MsgBuffer &cur_req_msgbuf = req_msgbufs[iter_req_i];
 
-        // Don't use very large requests because we drop a lot of packets
-        size_t req_pkts = (kSessionCredits * 2) +
-                          (context.fastrand.next_u32() % kSessionCredits);
-        size_t req_size = req_pkts * rpc->get_max_data_per_pkt();
+      // Don't use very large requests because we drop a lot of packets
+      size_t req_pkts = (kSessionCredits * 2) +
+                        (context.fastrand.next_u32() % kSessionCredits);
+      size_t req_size = req_pkts * rpc->get_max_data_per_pkt();
 
-        rpc->resize_msg_buffer(&cur_req_msgbuf, req_size);
-        memset(cur_req_msgbuf.buf, iter_req_i, req_size);
+      rpc->resize_msg_buffer(&cur_req_msgbuf, req_size);
+      memset(cur_req_msgbuf.buf, iter_req_i, req_size);
 
-        rpc->enqueue_request(session_num_arr[sess_i], kTestReqType,
-                             &cur_req_msgbuf, &resp_msgbufs[iter_req_i],
-                             cont_func, iter_req_i);
+      rpc->enqueue_request(context.session_num_arr[0], kTestReqType,
+                           &cur_req_msgbuf, &resp_msgbufs[iter_req_i],
+                           cont_func, iter_req_i);
 
-        iter_req_i++;
-      }
+      iter_req_i++;
     }
 
     // The default timeout for tests is 20 seconds. This test takes more time
     // because of packet drops.
     for (size_t i = 0; i < 5; i++) {
-      wait_for_rpc_resps_or_timeout(context, tot_reqs_per_iter,
-                                    nexus->freq_ghz);
-      if (context.num_rpc_resps == tot_reqs_per_iter) break;
+      wait_for_rpc_resps_or_timeout(context, config_num_rpcs, nexus->freq_ghz);
+      if (context.num_rpc_resps == config_num_rpcs) break;
     }
-    assert(context.num_rpc_resps == tot_reqs_per_iter);
+    assert(context.num_rpc_resps == config_num_rpcs);
   }
 
   // Free the MsgBuffers
   for (auto &req_msgbuf : req_msgbufs) rpc->free_msg_buffer(req_msgbuf);
   for (auto &resp_msgbuf : resp_msgbufs) rpc->free_msg_buffer(resp_msgbuf);
 
-  // Disconnect the sessions
-  for (size_t sess_i = 0; sess_i < config_num_sessions; sess_i++) {
-    rpc->destroy_session(session_num_arr[sess_i]);
-  }
-
+  rpc->destroy_session(context.session_num_arr[0]);
   rpc->run_event_loop(kTestEventLoopMs);
 
   // Free resources
@@ -159,55 +147,35 @@ void generic_test_func(Nexus *nexus, size_t) {
 void launch_helper() {
   auto &reg_info_vec =
       config_num_bg_threads == 0 ? reg_info_vec_fg : reg_info_vec_bg;
-  launch_server_client_threads(config_num_sessions, config_num_bg_threads,
+  launch_server_client_threads(1 /* num_sessions */, config_num_bg_threads,
                                generic_test_func, reg_info_vec,
                                ConnectServers::kFalse, kPktDropProb);
 }
 
 TEST(OneLargeRpc, Foreground) {
   config_num_iters = 1;
-  config_num_sessions = 1;
-  config_rpcs_per_session = 1;
+  config_num_rpcs = 1;
   config_num_bg_threads = 0;
   launch_helper();
 }
 
 TEST(OneLargeRpc, Background) {
   config_num_iters = 1;
-  config_num_sessions = 1;
-  config_rpcs_per_session = 1;
+  config_num_rpcs = 1;
   config_num_bg_threads = 1;
   launch_helper();
 }
 
 TEST(MultiLargeRpcOneSession, Foreground) {
   config_num_iters = 2;
-  config_num_sessions = 1;
-  config_rpcs_per_session = kSessionReqWindow;
+  config_num_rpcs = kSessionReqWindow;
   config_num_bg_threads = 0;
   launch_helper();
 }
 
 TEST(MultiLargeRpcOneSession, Background) {
   config_num_iters = 2;
-  config_num_sessions = 1;
-  config_rpcs_per_session = kSessionReqWindow;
-  config_num_bg_threads = 1;
-  launch_helper();
-}
-
-TEST(MultiLargeRpcMultiSession, Foreground) {
-  config_num_iters = 2;
-  config_num_sessions = 2;
-  config_rpcs_per_session = kSessionReqWindow;
-  config_num_bg_threads = 0;
-  launch_helper();
-}
-
-TEST(MultiLargeRpcMultiSession, Background) {
-  config_num_iters = 2;
-  config_num_sessions = 2;
-  config_rpcs_per_session = kSessionReqWindow;
+  config_num_rpcs = kSessionReqWindow;
   config_num_bg_threads = 1;
   launch_helper();
 }
