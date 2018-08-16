7c9c8734e6743115e94c5f882e1b69a0fe8cb493
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-16 18:47:31 -0400

    update centos package install script

--- scripts/centos/packages.sh
+++ scripts/centos/packages.sh
@@ -10,23 +10,28 @@ wget -P /tmp/ https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-rel
 sudo rpm -Uvh /tmp/epel-release*rpm
 
 ###
-### Required packages
+### Packages required for eRPC
 ###
-
 sudo yum -y install gcc-c++ cmake numactl-devel numactl bc gflags-devel \
   boost-devel gtest gtest-devel libpmem libpmem-devel
+
+###
+### Packages required to install Mellanox OFED
+###
+sudo yum -y install createrepo python2-devel elfutils-libelf-devel \
+  redhat-rpm-config rpm-build libxml2-python gtk2 \
+  gcc-gfortran atk tk tcl tcsh perl
 
 ###
 ### Optional convenience packages
 ###
 
 # Update vim
-sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
 sudo yum -y update vim*
 
 # General packages not specific to eRPC
-sudo yum -y install ripgrep htop memcached memcached-devel git-clang-format \
-  ctags-etags the_silver_searcher sloccount calc
+sudo yum -y install htop memcached memcached-devel ctags-etags \
+  the_silver_searcher sloccount calc
 
 # Fuzzy find configuration
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
