8eed10b74363755e02e775292fe37cca286d1e3b
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-20 16:23:15 -0600

    make separate directories for package scripts

---
+++ scripts/packages/ubuntu18/required.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Set up a fresh Ubuntu 18.04 box with only essential scripts for building the
+# eRPC library. This does not install application-specific or
+# dev environment packages.
+
+sudo apt update
+
+# Toolchain
+sudo apt -y install g++-8 cmake
+
+# General libs
+sudo apt -y install libnuma-dev libgflags-dev libgtest-dev
+(cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/)
+
+# Verbs drivers
+sudo apt -y install libibverbs-dev ibverbs-providers 
+
+# DPDK drivers
+sudo apt -y install dpdk libdpdk-dev dpdk-igb-uio-dkms
