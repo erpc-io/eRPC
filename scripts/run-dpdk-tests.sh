3ab6588f7e5a77f978e6655fb5feb8fd81f49526
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-23 20:14:46 -0400

    add a script to run each unit test in a separate process. this is useful for dpdk.

---
+++ scripts/run-dpdk-tests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Run all tests one by one in separate processes. This is needed because it is
+# difficult to repeatedly cleanup and reinitialize DPDK in one process.
+#
+# Run this script from the eRPC/ folder
+
+source $(dirname $0)/utils.sh
+
+exe_list=`ls build | grep test | tr '\n' ' '`
+blue "run-dpdk-tests: Found test executables:"
+echo "$exe_list"
+
+for exe in $exe_list; do
+  blue "run-dpdk-tests: Running test executable $exe"
+
+  # gtest_list_tests lists the tests as follows:
+  # TestCase1.
+  #   TestName1
+  #   TestName2
+  # TestCase2.
+  #   TestName3
+  # ...
+  # For gtest_filter, we need names like TestCase1.TestName1.
+  test_list=`./build/$exe --gtest_list_tests`
+
+  for word in $test_list; do
+    if [[ $word = *"." ]]; then
+      cur_test_name=$word
+    else
+      echo "run-dpdk-tests: Running test $cur_test_name$word"
+      sudo ./build/$exe --gtest_filter=$cur_test_name$word
+    fi
+  done
+done
