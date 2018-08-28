49a41331f3c45099be4252a2455cc4062790f025
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-27 19:05:34 -0500

    improve cloudlab dpdk setup script

--- scripts/setup_dpdk/cloudlab.sh
+++ scripts/setup_dpdk/cloudlab.sh
@@ -6,6 +6,11 @@ sudo modprobe uio
 sudo modprobe igb_uio
 
 # Bind CloudLab's experimental interface (10.*.*.*) to DPDK
+if [ ! -f ifname ]; then
+  echo "Error: scripts/setup-dpdk/ifname not found."
+  exit
+fi
+
 (cd $(dirname $0); rm ifname; g++ -std=c++11 -o ifname ifname.cc)
 exp_interfaces=`"$(dirname $0)"/ifname 10.`
 echo "Binding interfaces $exp_interfaces to DPDK"
