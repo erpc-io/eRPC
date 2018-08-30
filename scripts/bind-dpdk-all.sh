296c75df567b6c09bb102dc2f0360026f86c6611
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-30 15:37:37 -0400

    tiny fix to bind-dpdk-all.sh

--- scripts/bind-dpdk-all.sh
+++ scripts/bind-dpdk-all.sh
@@ -12,7 +12,7 @@ wait
 blue "Printing dpdk-devbind results on all nodes..."
 for ((i = 0; i < $autorun_num_processes; i++)); do
   ret=`ssh -oStrictHostKeyChecking=no ${autorun_name_list[$i]} "\
-    dpdk-devbind -s | grep unused=ixgbe"`
+    dpdk-devbind -s | grep drv=igb_uio"`
   echo "Machine ${autorun_name_list[$i]}: $ret"
   if [ -z "$ret" ]; then
     echo "No DPDK port available on ${autorun_name_list[$i]}"
