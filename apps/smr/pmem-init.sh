89e298c1d6d4ee165404d8645b9917f2e2c960d4
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-05 16:14:06 -0700

    add script to init pmem log for smr

---
+++ apps/smr/pmem-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This creates the persistent memory Raft log file (/mnt/pmem12/raft_log).
+#
+# This assumes that there are two pmem regions. Check with:
+#   sudo ipmctl show -region
+#
+# To initialize two regions:
+#   sudo ipmctl create -goal MemoryMode=0
+#   sudo reboot
+
+sudo ndctl create-namespace --mode fsdax --region 12
+sudo ndctl create-namespace --mode fsdax --region 13
+
+# Format an ext4 filesystem because the device contains garbage:
+sudo mkfs.ext4 /dev/pmem12
+
+sudo mkdir /mnt/pmem12
+sudo mount -o dax /dev/pmem12 /mnt/pmem12
+sudo chmod --recursive ugo+rw /mnt/pmem12
+fallocate -l 256G /mnt/pmem12/raft_log  # Some machines have 384 GB per NUMA
