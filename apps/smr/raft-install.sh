9ed8e7d85752625e50701c33137d2d19a202b189
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-06 16:26:37 -0700

    better raft install script

--- apps/smr/raft-install.sh
+++ apps/smr/raft-install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-# willemt/raft does not have an install command
-# Usage: Run in the willemt/raft folder
+#
+# willemt/raft's Makefile contains flags for code coverage that we don't need.
+# This compiles a more optimized library.
+#
+# willemt/raft does not have an install command. This does.
+
+rm -rf /tmp/raft
+git clone https://github.com/willemt/raft.git /tmp/raft
+
+cd /tmp/raft
+make CFLAGS='-Iinclude -Werror -Werror=return-type -Werror=uninitialized \
+  -Wcast-align -Wno-pointer-sign -fno-omit-frame-pointer -fno-common \
+  -fsigned-char -I CLinkedListQueue/ -g -O3 -fPIC -march=native'
+
 sudo mkdir -p /usr/local/include/raft
 sudo cp include/* /usr/local/include/raft/
 sudo cp libraft.a /usr/local/lib
+
 sudo ldconfig
