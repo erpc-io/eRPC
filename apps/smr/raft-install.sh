186682d4af1f76090ea26f36f1341198b2655398
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-06 19:18:42 -0700

    compile willemt/raft with DNDEBUG

--- apps/smr/raft-install.sh
+++ apps/smr/raft-install.sh
@@ -11,7 +11,7 @@ git clone https://github.com/willemt/raft.git /tmp/raft
 cd /tmp/raft
 make CFLAGS='-Iinclude -Werror -Werror=return-type -Werror=uninitialized \
   -Wcast-align -Wno-pointer-sign -fno-omit-frame-pointer -fno-common \
-  -fsigned-char -I CLinkedListQueue/ -g -O3 -fPIC -march=native'
+  -fsigned-char -I CLinkedListQueue/ -g -O3 -DNDEBUG -fPIC -march=native'
 
 sudo mkdir -p /usr/local/include/raft
 sudo cp include/* /usr/local/include/raft/
