ae1d502e0efd518aa1e7d2c79e788f3a0c0be94f
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-10-02 21:38:38 -0400

    improve docs

--- src/msg_buffer.h
+++ src/msg_buffer.h
@@ -13,16 +13,24 @@ class Session;
 template <typename T>
 class Rpc;
 
-/// A message buffer with magic-ful headers at the beginning and end
+/**
+ * @brief Applications store request and response messages in hugepage-backed
+ * buffers called message buffers. These buffers are registered with the NIC,
+ * allowing fast zero-copy transmission.
+ *
+ * A message buffer is allocated using Rpc::alloc_msg_buffer. Only its maximum
+ * size is specified during allocation. Later, users can resize the message
+ * buffer with Rpc::resize_msg_buffer to fit smaller requests or responses.
+ * Rpc::free_msg_buffer frees a message buffer.
+ *
+ * A message buffer is invalid if its #buf pointer is null.
+ */
 class MsgBuffer {
   friend class CTransport;
   friend class Rpc<CTransport>;
   friend class Session;
 
- public:
-  MsgBuffer() {}
-  ~MsgBuffer() {}
-
+ private:
   /// Return a pointer to the pre-appended packet header of this MsgBuffer
   inline pkthdr_t *get_pkthdr_0() const {
     return reinterpret_cast<pkthdr_t *>(buf - sizeof(pkthdr_t));
@@ -44,7 +52,6 @@ class MsgBuffer {
   inline bool is_rfr() const { return get_pkthdr_0()->is_rfr(); }
   inline uint64_t get_req_num() const { return get_pkthdr_0()->req_num; }
   inline uint64_t get_pkt_type() const { return get_pkthdr_0()->pkt_type; }
-  inline uint8_t get_req_type() const { return get_pkthdr_0()->req_type; }
   ///@}
 
   std::string get_pkthdr_str(size_t pkt_idx) const {
@@ -64,10 +71,6 @@ class MsgBuffer {
   inline bool is_buried() const {
     return (buf == nullptr && buffer.buf == nullptr);
   }
-
-  /// Return the current amount of data in the MsgBuffer. This can be smaller
-  /// than the maximum data capacity of the MsgBuffer.
-  inline size_t get_data_size() const { return data_size; }
 
   /// Get the packet size (i.e., including packet header) of a packet
   template <size_t kMaxDataPerPkt>
@@ -92,12 +95,12 @@ class MsgBuffer {
   /// The zeroth packet header is stored at \p buffer.buf. \p buffer must have
   /// space for at least \p max_data_bytes, and \p max_num_pkts packet headers.
   MsgBuffer(Buffer buffer, size_t max_data_size, size_t max_num_pkts)
-      : buf(buffer.buf + sizeof(pkthdr_t)),
-        buffer(buffer),
+      : buffer(buffer),
         max_data_size(max_data_size),
         data_size(max_data_size),
         max_num_pkts(max_num_pkts),
-        num_pkts(max_num_pkts) {
+        num_pkts(max_num_pkts),
+        buf(buffer.buf + sizeof(pkthdr_t)) {
     assert(buffer.buf != nullptr);  // buffer must be valid
     // data_size can be 0
     assert(max_num_pkts >= 1);
@@ -117,11 +120,11 @@ class MsgBuffer {
   /// setting \p buffer to invalid so that we know not to free it.
   /// \p pkt must have space for \p max_data_bytes and one packet header.
   MsgBuffer(pkthdr_t *pkthdr, size_t max_data_size)
-      : buf(reinterpret_cast<uint8_t *>(pkthdr) + sizeof(pkthdr_t)),
-        max_data_size(max_data_size),
+      : max_data_size(max_data_size),
         data_size(max_data_size),
         max_num_pkts(1),
-        num_pkts(1) {
+        num_pkts(1),
+        buf(reinterpret_cast<uint8_t *>(pkthdr) + sizeof(pkthdr_t)) {
     assert(pkthdr->check_magic());  // pkthdr is the zeroth header
     // max_data_size can be zero for control packets, so can't assert
 
@@ -137,17 +140,33 @@ class MsgBuffer {
   }
 
  public:
-  /// Pointer to the first *data* byte. (\p buffer.buf does not point to the
-  /// first data byte.) The MsgBuffer is invalid if this is null.
-  uint8_t *buf;
+  // The real constructors are private
+  MsgBuffer() {}
+  ~MsgBuffer() {}
+
+  /// Return the request type of this message buffer
+  inline uint8_t get_req_type() const { return get_pkthdr_0()->req_type; }
+
+  /**
+   * Return the current amount of data in this message buffer. This can be
+   * smaller than it's maximum data capacity due to resizing.
+   */
+  inline size_t get_data_size() const { return data_size; }
 
  private:
-  Buffer buffer;  ///< The (optional) backing hugepage Buffer
+  /// The optional backing hugepage buffer. buffer.buf points to the zeroth
+  /// packet header, i.e., not application data.
+  Buffer buffer;
 
   // Size info
   size_t max_data_size;  ///< Max data bytes in the MsgBuffer
   size_t data_size;      ///< Current data bytes in the MsgBuffer
   size_t max_num_pkts;   ///< Max number of packets in this MsgBuffer
   size_t num_pkts;       ///< Current number of packets in this MsgBuffer
+
+ public:
+  /// Pointer to the first application data byte. The message buffer is invalid
+  /// invalid if this is null.
+  uint8_t *buf;
 };
 }  // namespace erpc
