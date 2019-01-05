20b95ea2be6a854fc6f5c18fb71d9495ac247ac4
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-01-04 20:04:36 -0500

    add more pinger tests. all failing rn.

--- tests/util_tests/pinger_test.cc
+++ tests/util_tests/pinger_test.cc
@@ -55,11 +55,14 @@ TEST(PingerTest, URISplitTest) {
 }
 
 TEST(PingerTest, Client) {
-  std::vector<std::string> failed_uris;
   Pinger pinger(kTestLocalHostname, kTestLocalSmUdpPort, kTestFreqGhz,
                 kTestMachineFailureTimeoutMs);
-  pinger.ping_udp_client.enable_recording();
+  std::vector<std::string> failed_uris;
 
+  pinger.ping_udp_client.enable_recording();
+  std::vector<SmPkt> &sent_vec = pinger.ping_udp_client.sent_vec;
+
+  // The client will sent actual UDP packets, so we need valid addresses
   pinger.unlocked_add_remote_server("127.0.0.1:1");
   pinger.unlocked_add_remote_server("127.0.0.1:2");
   pinger.unlocked_add_remote_server("localhost:2");
@@ -69,7 +72,6 @@ TEST(PingerTest, Client) {
   usleep(2 * to_usec(pinger.ping_send_delta_tsc, kTestFreqGhz));  // wiggle-room
   pinger.do_one(failed_uris);
 
-  const std::vector<SmPkt> &sent_vec = pinger.ping_udp_client.sent_vec;
   assert(sent_vec.size() >= 3);
   std::set<std::string> SP;  // Sent pings
 
@@ -88,6 +90,45 @@ TEST(PingerTest, Client) {
   assert(str_vec_contains(failed_uris, "127.0.0.1:1"));
   assert(str_vec_contains(failed_uris, "127.0.0.1:2"));
   assert(str_vec_contains(failed_uris, "localhost:2"));
+
+  // All servers have failed, so no pings should be sent from now
+  sent_vec.clear();
+  failed_uris.clear();
+  usleep(2 * kTestMachineFailureTimeoutMs * 1000);  // x2 for wiggle-room
+  pinger.do_one(failed_uris);
+  assert(sent_vec.empty());
+  assert(failed_uris.empty());
+}
+
+TEST(PingerTest, Server) {
+  Pinger pinger(kTestLocalHostname, kTestLocalSmUdpPort, kTestFreqGhz,
+                kTestMachineFailureTimeoutMs);
+  std::vector<std::string> failed_uris;
+
+  pinger.ping_udp_client.enable_recording();
+
+  // The server never sends pings, so any addresses are fine
+  pinger.unlocked_add_remote_client("client_1:1");
+  pinger.unlocked_add_remote_client("client_1:2");
+  pinger.unlocked_add_remote_client("client_2:3");
+
+  // Test failure timeout
+  usleep(2 * kTestMachineFailureTimeoutMs * 1000);  // x2 for wiggle-room
+  pinger.do_one(failed_uris);
+
+  assert(failed_uris.size() == 3);
+  assert(str_vec_contains(failed_uris, "client_1:1"));
+  assert(str_vec_contains(failed_uris, "client_1:2"));
+  assert(str_vec_contains(failed_uris, "client_2:3"));
+
+  // The server should never send pings
+  assert(pinger.ping_udp_client.sent_vec.empty());
+
+  // All clients have failed, so no failures should be detected from now
+  failed_uris.clear();
+  usleep(2 * kTestMachineFailureTimeoutMs * 1000);  // x2 for wiggle-room
+  pinger.do_one(failed_uris);
+  assert(failed_uris.empty());
 }
 
 int main(int argc, char **argv) {
