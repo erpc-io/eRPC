31f8d055a047936f07ec71dbc3b9c36bb36ca333
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-05-30 12:09:04 -0600

    improve xl170 topology detection script

--- scripts/topo/xl170-topo.sh
+++ scripts/topo/xl170-topo.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Prints the nodes under each switch in CloudLab's xl170 cluster
-source $(dirname $0)/utils.sh
 if [ "$#" -ne 1 ]; then
   echo "Illegal number of parameters"
 	echo "Usage: ./xl170-topo.sh <number of nodes to look up>"
@@ -18,12 +17,12 @@ for ((i = 1; i <= $1; i++)); do
   hostname="akalianode-$i.RDMA.ron-PG0.utah.cloudlab.us"
 
   # Get the HP node ID (e.g., hp012 from hp012.utah.cloudlab.us)
-	hpnode_id=`ssh -oStrictHostKeyChecking=no $hostname \
+  hpnode_id=`ssh -oStrictHostKeyChecking=no $hostname \
     "hostname -A | cut -d '.' -f 1"`
 
   # Ignore if node is bad
   if [[ $bad_nodes == *"$hpnode_id"*  ]]; then
-    blue "Ignoring bad node $hpnode_id ($hostname)"
+    echo "Ignoring bad node $hpnode_id ($hostname)"
   else
     hpnode_id=`echo $hpnode_id | sed 's/hp0*//g'` # Trim leading hp and zeros
     switch_id=$(( ($hpnode_id - 1) / 40 ))
@@ -40,7 +39,7 @@ echo ""
 for ((switch_i = 0; switch_i < 5; switch_i++)); do
   nodes_file=$topodir/switch_$switch_i
   count=`cat temp | grep " $switch_i" | wc -l`
-  blue "Under switch $switch_i ($count nodes):"
+  echo "Under switch $switch_i ($count nodes):"
   cat temp | grep " $switch_i" | cut -d' ' -f 1 | sort -n > $nodes_file
   sed -e 's/$/ 31850 0/' -i $nodes_file  # Append UDP port and NUMA node
   cat $nodes_file
