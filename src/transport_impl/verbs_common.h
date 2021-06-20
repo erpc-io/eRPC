eff75243cbe97ed615534c797df720b5ad8070ad
Anuj Kalia <anujkaliaiitd@gmail.com>
2021-06-20 13:35:15 -0700

    MemRegInfo -> mem_reg_info_t

--- src/transport_impl/verbs_common.h
+++ src/transport_impl/verbs_common.h
@@ -54,19 +54,19 @@ static std::string link_layer_str(uint8_t link_layer) {
  *
  * @throw runtime_error if memory registration fails
  */
-static Transport::MemRegInfo ibv_reg_mr_wrapper(struct ibv_pd *pd, void *buf,
-                                                size_t size) {
+static Transport::mem_reg_info ibv_reg_mr_wrapper(struct ibv_pd *pd, void *buf,
+                                                  size_t size) {
   struct ibv_mr *mr = ibv_reg_mr(pd, buf, size, IBV_ACCESS_LOCAL_WRITE);
   rt_assert(mr != nullptr, "Failed to register mr.");
 
   ERPC_INFO("Registered %zu MB (lkey = %u)\n", size / MB(1), mr->lkey);
-  return Transport::MemRegInfo(mr, mr->lkey);
+  return Transport::mem_reg_info(mr, mr->lkey);
 }
 
 /// A function wrapper used to generate a verbs transport's memory
 /// deregistration function
-static void ibv_dereg_mr_wrapper(Transport::MemRegInfo mr) {
-  auto *ib_mr = reinterpret_cast<struct ibv_mr *>(mr.transport_mr);
+static void ibv_dereg_mr_wrapper(Transport::mem_reg_info mr) {
+  auto *ib_mr = reinterpret_cast<struct ibv_mr *>(mr.transport_mr_);
   size_t size = ib_mr->length;
   uint32_t lkey = ib_mr->lkey;
 
