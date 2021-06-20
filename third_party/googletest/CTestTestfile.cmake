f2b18d4f4e7b6d86984499c7177f15696f646259
Anuj Kalia <anujkaliaiitd@gmail.com>
2021-06-20 12:44:19 -0700

    Delete temporary files from third_party/googletest

--- third_party/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ankalia/eRPC/third_party/googletest
-# Build directory: /home/ankalia/eRPC/third_party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- third_party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ankalia/eRPC/third_party/googletest/googletest
-# Build directory: /home/ankalia/eRPC/third_party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
