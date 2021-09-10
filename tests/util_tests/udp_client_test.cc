578a253191a08bc157b2538a978c518b127a1b64
Anuj Kalia <anujkaliaiitd@gmail.com>
2021-09-10 10:50:56 -0700

    Initialize int message in udp_client_test

--- tests/util_tests/udp_client_test.cc
+++ tests/util_tests/udp_client_test.cc
@@ -3,21 +3,21 @@
 
 TEST(UdpClientTest, LocalhostWrong) {
   erpc::UDPClient<int> udp_client;
-  int msg;
+  int msg = 33;
   size_t ret = udp_client.send("localhost", 31850, msg);
   ASSERT_EQ(ret, SIZE_MAX);  // The name localhost is not supported as a remote
 }
 
 TEST(UdpClientTest, LocalhostRight) {
   erpc::UDPClient<int> udp_client;
-  int msg;
+  int msg = 33;
   size_t ret = udp_client.send("127.0.0.1", 31850, msg);
   ASSERT_EQ(ret, sizeof(int));
 }
 
 TEST(UdpClientTest, InvalidRemote) {
   erpc::UDPClient<int> udp_client;
-  int msg;
+  int msg = 33;
   size_t ret = udp_client.send("randomhostnamelikelyinvalid9000", 31850, msg);
   ASSERT_EQ(ret, SIZE_MAX);
 }
